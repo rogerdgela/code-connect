63398bd72d2dbffad38d4c9dd2bcc745b3416f22
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-05-19 16:45:16 -0300

    Criando o componente Aside

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,16 @@
+import "./globals.css";
+import { Aside } from "@/components/Aside";
+
 export const metadata = {
   title: "Code Connect",
-  description: "Uma plataforma para conectar programadores",
+  description: "Uma rede social para conectar Devs!",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body>
+        <Aside />
         {children}
       </body>
     </html>
