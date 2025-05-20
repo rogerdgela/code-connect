54cc6ef0114e9f4fd050d27c4cb19a6a8c60afc9
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-05-20 17:11:42 -0300

    Componente Image do Next.js

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,9 +1,13 @@
 import styles from "./aside.module.css";
+import logo from "./logo.png";
+import Image from "next/image";
+
 
 export const Aside = () => {
     return (
         <aside className={styles.aside}>
-            <img src="/logo.png" alt="Logo Code Connect" />
+            {/* <img src="/logo.png" alt="Logo Code Connect" /> */}
+            <Image src={logo} alt="Logo Code Connect" />
         </aside>
     )
 }
