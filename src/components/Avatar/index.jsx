52106e5daad507c3b66a69c95892611d726c0fec
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-05-26 17:30:29 -0300

    Estruturando de forma semântica

---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+export const Avatar = ({ name, imageSrc }) => {
+    return (
+        <ul>
+            <li>
+                <Image src={imageSrc} width={32} height={32} />
+            </li>
+            <li>
+                @{name}
+            </li>
+        </ul>
+    )
+}
+
---
+++ src/components/CardPost/index.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Avatar } from "../Avatar";
+
+export const CardPost = ({ post }) => {
+    return (
+        <article>
+            <header>
+                <figure>
+                    <Image src={post.cover} alt={post.title} width={438} height={133} />
+                </figure>
+            </header>
+            <section>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </section>
+            <footer>
+                <Avatar 
+                    imageSrc={post.author.avatar}
+                    name={post.author.name}
+                />
+            </footer>
+        </article>
+    )
+}
+
