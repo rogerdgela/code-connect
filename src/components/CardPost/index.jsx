d58e76692519d1cd42ae2651ae8d8f87d738969d
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-06-09 16:50:24 -0300

    Criando função fetch

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,30 +1,30 @@
-import Image from "next/image";
-import { Avatar } from "../Avatar";
+import Image from "next/image"
+import { Avatar } from "../Avatar"
+import styles from './cardpost.module.css'
 
 export const CardPost = ({ post }) => {
     return (
-        <article>
-            <header>
+        <article className={styles.card}>
+            <header className={styles.header}>
                 <figure>
-                    <Image
-                        src={post.cover}
-                        width={438}
-                        height={133}
-                        alt={`Capa do post de titulo: ${post.title}`}
+                    <Image 
+                        src={post.cover} 
+                        width={438} 
+                        height={133} 
+                        alt={`Capa do post de titulo: ${post.title}`} 
                     />
                 </figure>
             </header>
-            <section>
+            <section className={styles.body}>
                 <h2>{post.title}</h2>
                 <p>{post.body}</p>
             </section>
-            <footer>
-                <Avatar
-                    imageSrc={post.author.avatar}
-                    name={post.author.name}
+            <footer className={styles.footer}>
+                <Avatar 
+                    imageSrc={post.author.avatar} 
+                    name={post.author.username}
                 />
             </footer>
         </article>
     )
 }
-
