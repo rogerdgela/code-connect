bcf8a5d0548a66967bd51433a79341ed52127cef
DESKTOP-EF0P7ET\Motors BR Roger <dgelask8@gmail.com>
2025-06-10 16:53:10 -0300

    Escrevendo logs

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import { CardPost } from "@/components/CardPost";
+import logger from "@/logger";
 
 
 async function getAllPosts() {
   const response = await fetch("http://localhost:3042/posts");
   if (!response.ok) {
-    console.log("Algo deu errado ao buscar os posts");
+    logger.error("Algo deu errado ao buscar os posts");
+    return [];
   }
+
+  logger.info("Posts obstidos com sucesso");
 
   return response.json();
 }
