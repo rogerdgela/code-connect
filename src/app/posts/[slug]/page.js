021b930f872af323099fed4fb750604f35b1834e
Rogerio Silva <roger.alves.silva84@gmail.com>
2025-07-03 16:56:23 -0300

    Atualizando logs de sucesso na obtenção de posts e ajustando a estrutura do componente CardPost para incluir links e novos estilos.

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,9 +1,32 @@
-const PagePost = () => {
+import logger from "@/logger";
+
+async function getPostBySlug(slug) {
+  const response = await fetch(`http://localhost:3042/posts?slug=${slug}`);
+  if (!response.ok) {
+    logger.error("Algo deu errado ao buscar os posts");
+    return {};
+  }
+
+  logger.info("Post obtido com sucesso");
+
+  const data = await response.json();
+  if (!data || data.length === 0) {
+    return {};
+  }
+  return data[0];
+}
+
+const PagePost = async ({ params }) => {
+  const post = await getPostBySlug(params.slug);
+
+  if (!post) {
+    return <p>Post não encontrado</p>;
+  }
+
   return (
-    <div>       
-        <h1>Posts</h1>
-        <p>Esta é a página de posts.</p>
-        <p>Você pode navegar pelos posts aqui.</p>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 }
