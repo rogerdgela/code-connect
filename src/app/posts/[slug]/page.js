f368544d2299e1cfa59585a36a14a9790553ff0c
Rogerio Silva <roger.alves.silva84@gmail.com>
2025-07-03 16:44:44 -0300

    Adicionando componente PagePost com estrutura básica e textos de navegação.

---
+++ src/app/posts/[slug]/page.js
@@ -0,0 +1,11 @@
+const PagePost = () => {
+  return (
+    <div>       
+        <h1>Posts</h1>
+        <p>Esta é a página de posts.</p>
+        <p>Você pode navegar pelos posts aqui.</p>
+    </div>
+  );
+}
+
+export default PagePost;
